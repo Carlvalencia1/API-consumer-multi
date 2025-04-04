f1c5f6fe5427373599f84829a3cb4e9005f73b06
Carlvalencia1 <233428@ids.upchiapas.edu.mx>
2025-04-04 05:44:56 -0600

    add: MySql

--- src/features/patients/infrastructure/adapters/MYSQL.go
+++ src/features/patients/infrastructure/adapters/MYSQL.go
@@ -1,1 +1,28 @@
 package adapters
+
+import (
+    "apiconsumer/src/core"
+	"database/sql"
+)
+
+type MYSQL struct {
+	conn *sql.DB
+}
+
+func NewMysql() *MYSQL {
+	conn := core.NewMysql()
+	return &MYSQL{
+		conn: conn,
+	}
+
+}
+
+func (m *MYSQL) FindID(id int) (error) {
+	result := m.conn.QueryRow("SELECT id FROM patients WHERE id = ?", id)
+
+	if err := result.Scan(&id); err != nil {
+		return err
+	}
+	return nil
+}
+
